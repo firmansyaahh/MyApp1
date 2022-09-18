4a493faa7f246c1d19d221f6171e71e3e0da28e7
firmansyaahh <firs070801@gmail.com>
2022-09-18 18:00:14 +0700

    tesawal

---
+++ index.tsx
@@ -0,0 +1,12 @@
+// export class Computer {
+//     public merk: String;
+//     public prossesor: String;
+//     public ram: Number;
+
+//     description(){
+//         return 'Laptop merk ${this.merk} memiliki prosessor ${this.prossesor}
+//         dengan dukungan RAM berkapasitas ${this.ram}GB.'
+      
+//     }
+
+// }
